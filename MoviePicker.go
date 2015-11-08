39f28bb4b2c365fd1338221c7b43b9e46f5672ea
doctori <lerouge.pierre@gmail.com>
2015-11-08 18:13:44 +0100

    cleaned stuffs

--- MoviePicker.go
+++ MoviePicker.go
@@ -53,12 +53,7 @@ func main() {
 
 // Find Films on OMDB
 func searchOMDB(film string) {
-		//
-	// Struct to hold error response
-	//
-	e := struct {
-		Message string
-	}{}
+
 	// Define Search query as a URL keys and values
 	search := url.Values{}
 	search.Set("s",film)
@@ -68,48 +63,23 @@ func searchOMDB(film string) {
 	result := struct{
 		Results 		[]OMDBFilmSearchResult `json:"Search"`
 	}{}
-
-	// Start Session
-  	s := napping.Session{}
+  	
   	url := "https://omdbapi.com/"
-  	fmt.Println("URL:>", url)
-  
-  	resp, err := s.Get(url,&search,&result,nil)
-  	if err != nil {
-  		log.Fatal(err)
-  	}
-  
-  //
-  // Process Response 
-  //
-  println("")
-  fmt.Println("Response Status:",resp.Status())
-  
-  if resp.Status() == 200 {
-		// fmt.Printf("Result: %s\n\n", resp.response)
-		// resp.Unmarshal(&e)
-		// Iterate through the results
-		for index,film := range result.Results {
-			fmt.Println("Index :",index)
-			fmt.Println("Title :", film.Title)
-			fmt.Println("imdbID : ",film.IMDBID)
-		}
-	} else {
-		fmt.Println("Bad response status from OMDB server")
-		fmt.Printf("\t Status:  %v\n", resp.Status())
-		fmt.Printf("\t Message: %v\n", e.Message)
+  	getRESTResponse(url,&search,&result)
+	// Iterate through the results
+	for index,film := range result.Results {
+		fmt.Println("Index :",index)
+		fmt.Println("Title :", film.Title)
+		fmt.Println("imdbID : ",film.IMDBID)
 	}
 }
 
+func getOMDBDetails(IMDBID string){
+
+}
 // Find Films on TMDB
 
 func searchTMDB(film string) {
-	//
-	// Struct to hold error response
-	//
-	e := struct {
-		Message string
-	}{}
 	
 	// Define Search query as a URL keys and values
 	search := url.Values{}
@@ -124,37 +94,38 @@ func searchTMDB(film string) {
 	}{}
 	//Hope that works !
 	
-	
-	
-  // Start Session
-  s := napping.Session{}
-  url := "https://api.themoviedb.org/3/search/movie"
-  fmt.Println("URL:>", url)
+  	url := "https://api.themoviedb.org/3/search/movie"
+  	getRESTResponse(url,&search,&result)
   
-  resp, err := s.Get(url,&search,&result,nil)
-  if err != nil {
-  	log.Fatal(err)
-  }
-  
-  //
-  // Process Response 
-  //
-  println("")
-  fmt.Println("Response Status:",resp.Status())
-  
-  if resp.Status() == 200 {
-		// fmt.Printf("Result: %s\n\n", resp.response)
-		// resp.Unmarshal(&e)
-		// Iterate through the results
-		for index,film := range result.Results {
-			fmt.Println("Index :",index)
-			fmt.Println("Title :", film.Title)
-			fmt.Println("Note : ",film.VoteAverage)
-		}
-	} else {
-		fmt.Println("Bad response status from TMDB server")
-		fmt.Printf("\t Status:  %v\n", resp.Status())
-		fmt.Printf("\t Message: %v\n", e.Message)
+	// Iterate through the results
+	for index,film := range result.Results {
+		fmt.Println("Index :",index)
+		fmt.Println("Title :", film.Title)
+		fmt.Println("Note : ",film.VoteAverage)
 	}
 
+}
+
+func getRESTResponse(url string, p *url.Values, result interface{}){
+	//
+	// Struct to hold error response
+	//
+	e := struct {
+		Message string
+	}{}
+
+	log.Println("URL => ", url)
+	s := napping.Session{}
+	resp, err := s.Get(url,p,result,e)
+	if err != nil {
+  		log.Fatal(err)
+  	}
+  	log.Println("Response Status:",resp.Status())
+  	if resp.Status() == 200 {
+  		return
+  	} else {
+  		log.Fatal("Resource could not be fetched")
+		log.Fatal("\t Status:  %v\n", resp.Status())
+		log.Fatal("\t Message: %v\n", e.Message)
+  	}
 }
